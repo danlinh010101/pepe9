77383296531de306ea63362aa9f63233a16d9c29
lekay955 <lekay955@gmail.com>
2026-07-30 09:54:55 +0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import { CursorGlow } from '@/components/CursorGlow';
-import { HeroRevealTransition } from '@/components/HeroRevealTransition';
-import { Impressions } from '@/components/Impressions';
-import { GlobalFollowers } from '@/components/GlobalFollowers';
-import { Tokenomics } from '@/components/Tokenomics';
-import { HowToBuy } from '@/components/HowToBuy';
-import { Community, Footer } from '@/components/Community';
-import { FAQ } from '@/components/FAQ';
-import { useLenis } from '@/hooks/useLenis';
-
-function App() {
-  useLenis();
-
-  return (
-    <div className="bg-black min-h-screen">
-      <CursorGlow />
-      <HeroRevealTransition />
-      <Impressions />
-      <GlobalFollowers />
-      <Tokenomics />
-      <HowToBuy />
-      <Community />
-      <FAQ />
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
+  import { CursorGlow } from '@/components/CursorGlow';
+  import { HeroRevealTransition } from '@/components/HeroRevealTransition';
+  import { Impressions } from '@/components/Impressions';
+  import { GlobalFollowers } from '@/components/GlobalFollowers';
+  import { Tokenomics } from '@/components/Tokenomics';
+  import { HowToBuy } from '@/components/HowToBuy';
+  import { Community, Footer } from '@/components/Community';
+  import { FAQ } from '@/components/FAQ';
+  import { useLenis } from '@/hooks/useLenis';
+  
+  function App() {
+    useLenis();
+  
+    return (
+      <div className="bg-black min-h-screen">
+        <CursorGlow />
+        <HeroRevealTransition />
+        <Impressions />
+        <GlobalFollowers />
+        <Tokenomics />
+        <HowToBuy />
+        <Community />
+        <FAQ />
+        <Footer />
+      </div>
+    );
+  }
+  
+  export default App;
